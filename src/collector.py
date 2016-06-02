07717a6ae1ab32352ae2a0518f959612bc023542
Souren <souren@papazian.info>
2016-06-02 00:10:24 -0700

    updated word collector and spellchecker

--- src/collector.py
+++ src/collector.py
@@ -1,29 +1,35 @@
 #!/usr/bin/python
 # This Python file uses the following encoding: utf-8
-import sys, os, re, unicodedata, codecs, operator, collections, glob
+import sys, os, re, unicodedata, codecs, operator, collections, time
 
 def getWords(text):
     '''
-    Match all Armenian words
+    Match all armenian words
+    pattern: armenian string
+    0531 - Ա, 0556 - Ֆ, 0561 - ա, 0587 - և
     '''
-    # All Armenian words: 0531 - Ա, 0556 - Ֆ, 0561 - ա, 0587 - և
-    armenian_word = re.compile(ur'[\u0531-\u0556\u0561-\u0587]+', re.UNICODE)
+    pattern = ur'[\u0531-\u0556\u0561-\u0587]+'
+    armenian_word = re.compile(pattern, re.UNICODE)
     return re.findall(armenian_word, text)
 
 def removePunc(text):
     '''
     Remove puncation
+    pattern: All Armenian punctuation
+    055E - QUESTION MARK    055C - EXCLAMATION MARK
+    058A - HYPHEN           055B - EMPHASIS MARK
     '''
-    # All Armenian punctuation: 055E - QUESTION MARK, 055C - EXCLAMATION MARK, 058A - HYPHEN, 055B - EMPHASIS MARK
-    punctuation = re.compile(ur'[\u055E\u055C\u058A\u055B]', re.UNICODE)
+    pattern = ur'[\u055E\u055C\u058A\u055B]'
+    punctuation = re.compile(pattern, re.UNICODE)
     return re.sub(punctuation, '', text)
 
 def removeProper(text):
     '''
     Remove all words that are capital and not after punctuation
+    pattern: Capitalized armenian word not after punctuation
     '''
-    # Capital Armenian word not after punctuation
-    proper_word = re.compile(ur'((?<![^―:։—\n\r․…–])\s[\u0531-\u0556][\u0561-\u0587]+)', re.UNICODE)
+    pattern = ur'((?<![^―:։—\n\r․…–])\s[\u0531-\u0556][\u0561-\u0587]+)'
+    proper_word = re.compile(pattern, re.UNICODE)
     return re.sub(proper_word, ' ', text)
 
 def makeDict(features):
@@ -42,21 +48,28 @@ def printDict(dict):
     '''
     s_list = sorted(dict.items(), key=operator.itemgetter(1))
     for k,v in s_list:
-        print '{0:5d}\t{1}'.format(v, k.encode('utf-8'))
+        print '%7d\t%s' % (v, k.encode('utf-8'))
 
 def main():
     # Get directory
     if len(sys.argv) == 2:
-        directory = sys.argv[1]
-        print directory
+        path = sys.argv[1]
     else:
         print "Usage: ./collector.py <dir>"
         print "Example: ./collector.py books/*.txt"
 
+    logfile = open("log_%s.txt" % time.time(), 'w+')
+
     # Collect words
     words = []
-    for filename in glob.glob(directory):
-        print filename
+
+    # get all .txt file paths recursively
+    files = [os.path.join(dirpath, f)
+    for dirpath, dirnames, files in os.walk(path)
+    for f in files if f.endswith('.txt')]
+
+    for filename in files:
+        logfile.write(filename + "\n")
         f = codecs.open(filename, encoding='utf-8')
         f.seek(0)
         text = f.read()
---
+++ src/spellchecker.py
@@ -0,0 +1,71 @@
+#!/usr/bin/python
+# This Python file uses the following encoding: utf-8
+import re, collections, unicodedata, codecs, operator, os
+
+''' Helpers '''
+
+# Print unicode string
+def printU(text):
+  print text.encode('utf-8');
+
+# Print array of unicode strings
+def printUs(texts):
+  for s in texts:
+    print s.encode('utf-8');
+
+''' Spellchecker '''
+
+class spellchecker:
+    def __init__(self):
+        self.armenian = re.compile(ur'[\u0531-\u0556\u0561-\u0587]+', re.UNICODE)
+        self.alphabet = [u'\u0561', u'\u0562', u'\u0563', u'\u0564',
+                         u'\u0565', u'\u0566', u'\u0567', u'\u0568',
+                         u'\u0569', u'\u056A', u'\u056B', u'\u056C',
+                         u'\u056D', u'\u056E', u'\u056F', u'\u0570',
+                         u'\u0571', u'\u0572', u'\u0573', u'\u0574',
+                         u'\u0575', u'\u0576', u'\u0577', u'\u0578',
+                         u'\u0579', u'\u057A', u'\u057B', u'\u057C',
+                         u'\u057D', u'\u057E', u'\u057F', u'\u0580',
+                         u'\u0581', u'\u0582', u'\u0583', u'\u0584',
+                         u'\u0585', u'\u0586', u'\u0587']
+        self.NWORDS = collections.defaultdict(int)
+
+    def train(self, dict_file):
+        # Read in words and their counts
+        with open(dict_file, 'r') as f:
+            for line in f:
+                if line:
+                    count, word = line.split()
+                    self.NWORDS[word] += 1
+
+    def edits1(self, word):
+        splits     = [(word[:i], word[i:]) for i in range(len(word) + 1)]
+        deletes    = [a + b[1:] for a, b in splits if b]
+        transposes = [a + b[1] + b[0] + b[2:] for a, b in splits if len(b)>1]
+        replaces   = [a + c + b[1:] for a, b in splits for c in self.alphabet if b]
+        inserts    = [a + c + b     for a, b in splits for c in self.alphabet]
+        return set(deletes + transposes + replaces + inserts)
+
+    def known_edits2(self, word):
+        return set(e2 for e1 in self.edits1(word) for e2 in self.edits1(e1) if e2 in self.NWORDS)
+
+    def known(self, words): return set(w for w in words if w in self.NWORDS)
+
+    def correct(self, word):
+        word = word.decode('utf-8');
+        candidates = self.known([word]) or self.known(self.edits1(word)) or self.known_edits2(word) or [word]
+        return max(candidates, key=self.NWORDS.get)
+
+    def correctMatch(self, match):
+        return self.correct(match.group().encode('utf-8'))
+
+    def correctText(self, text):
+        text = text.decode('utf-8');
+        return re.sub(self.armenian, self.correctMatch, text);
+
+''' Main '''
+
+if __name__ == "__main__":
+    pass
+    #sp = spellchecker();
+    #printU(sp.correctText('դժղոխքի: աղղմկում'))
